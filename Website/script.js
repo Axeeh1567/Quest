6cf99dc090ce8400df989547e0a546e77d1018ea
Axeeh1567 <seikailove62@gmail.com>
2026-04-30 16:25:29 +0700

    Delete Website directory

--- Website/script.js
+++
@@ -1,6 +0,0 @@
-function downloadSecret() {
-  const link = document.createElement("a");
-  link.href = "Secret.zip"; // file asli ada di folder project
-  link.download = "Secret.zip";
-  link.click();
-}
